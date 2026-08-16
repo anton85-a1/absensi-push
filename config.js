6924e05bde0a521bd610a742a44105be6f0fdfa9
anton85-a1 <anton85.a1@gmail.com>
2026-08-17 06:50:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,6 +8,6 @@
 // ============================================================
 
 window.PUSH_CONFIG = Object.freeze({
-  ONESIGNAL_APP_ID: 'ISI_ONESIGNAL_APP_ID',
+  ONESIGNAL_APP_ID: '64ecf8f0-9b81-4b56-b6a6-bb0cc95754c6',
   GAS_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbwB1O84zB2mga8ujkKSXqrGI3UHJnsb9JZTU9wH2kz5cBQSR5BLc0W2Tb7KYSW8dcLY/exec'
 });
